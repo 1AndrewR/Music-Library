391c2afa6a9ac85ab76806e364f2957537553350
Andrew Roth <a.roth.80@hotmail.com>
2024-11-08 21:17:54 -0800

    printPlaylists refactored into method with this

--- library.js
+++ library.js
@@ -1,28 +1,14 @@
 const library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             }
-};
+  tracks: { 
+    t01: { id: "t01", name: "Code Monkey", artist: "Jonathan Coulton", album: "Thing a Week Three" },
+    t02: { id: "t02", name: "Model View Controller", artist: "James Dempsey", album: "WWDC 2003"},
+    t03: { id: "t03", name: "Four Thirty-Three", artist: "John Cage", album: "Woodstock 1952"}
+  },
 
+  playlists: { 
+    p01: { id: "p01", name: "Coding Music", tracks: ["t01", "t02"] },
+    p02: { id: "p02", name: "Other Playlist", tracks: ["t03"] }
+  },
 /////////////////////////////
 // FUNCTIONS TO IMPLEMENT:
 /////////////////////////////
@@ -30,12 +16,12 @@ const library = {
 // prints a list of all playlists, in the form:
 // p01: Coding Music - 2 tracks
 // p02: Other Playlist - 1 tracks
-const printPlaylists = function() {
-  for (let playlistId in library.playlists) {
-    const playlist = library.playlists[playlistId];
+printPlaylists: function() {
+  for (let playlistId in this.playlists) {
+    const playlist = this.playlists[playlistId];
     console.log(`${playlist.id}: ${playlist.name} - ${playlist.tracks.length} tracks`);
   }
-};
+},
 printPlaylists();
 
 // prints a list of all tracks, using the following format:
